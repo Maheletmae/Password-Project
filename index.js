fdd733cbbdafe07a37000f3691d42570fe6c34dd
Maheletmae <88266126+Maheletmae@users.noreply.github.com>
2024-06-08 11:15:11 +0200

    adding npm module body-parser and express and initial javascript code
    - importing the express and body-parser modules
    - adding the code from the body-parser module to get the right file path on all servers
    - making the app listen to the port 3000
    - get an post request to get the right html file depending on the situation (submit button and initial view)

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 //The password is ILoveProgramming
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+app.use(bodyParser.urlencoded({ extended: true })); // to be able to read body
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  console.log("Hello");
+  res.sendFile(__dirname + "/public/secret.html");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
